97bfaee8d0b76639479c8688124fa9037a5ec034
ezzarnihboss <aliezzarw@gmail.com>
2026-08-02 02:41:14 +0700

    perf: reduce mongo connection timeout to 3s for fast startup

--- server/routes/connections.go
+++ server/routes/connections.go
@@ -23,7 +23,7 @@ func DBinstance() *mongo.Client {
 		MongoDb = "mongodb://localhost:27017"
 	}
 
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 	defer cancel()
 
 	client, err := mongo.Connect(ctx, options.Client().ApplyURI(MongoDb))
